1b4274b4ff1b7e0380937f909b86402ed4149278
Travis Veazey <travisvz@gmail.com>
2016-02-03 12:48:19 -0900

    Fancier display of memory/disk

--- status/utils.py
+++ status/utils.py
@@ -20,10 +20,14 @@ def get_system_detail():
 def get_system_memory():
     psmem = psutil.virtual_memory()
 
+    # What's "used" logically and what's "used" in the output don't really
+    # mesh, so we calculate "used" as total less available.
     mem = {
-            'free': psmem.available,
             'total': psmem.total,
-            'percent': round(psmem.available/psmem.total*100, 1),
+            'free': psmem.available,
+            'used': psmem.total-psmem.available,
+            'percentfree': round(psmem.available/psmem.total*100, 1),
+            'percentused': round(100-psmem.available/psmem.total*100, 1),
             }
     return mem
 
@@ -32,9 +36,11 @@ def get_disk_usage():
     psdisk = psutil.disk_usage('/')
 
     disk = {
-            'free': psdisk.free,
             'total': psdisk.total,
-            'percent': round(psdisk.free/psdisk.total*100, 1),
+            'free': psdisk.free,
+            'used': psdisk.used,
+            'percentfree': round(psdisk.free/psdisk.total*100, 1),
+            'percentused': round(psdisk.used/psdisk.total*100, 1),
             }
     return disk
 
