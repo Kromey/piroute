ad11f8dfd5385e8cbd8a6f5cf2dc1cc1f0d3724c
Travis Veazey <travisvz@gmail.com>
2016-02-03 15:12:13 -0900

    Use jQuery for progress bars
    No more inline styles for widths, all handled magically via jQuery instead. Also some reorganization of the .js file.

--- htdocs/static/js/firewall.js
+++ htdocs/static/js/firewall.js
@@ -1,49 +1,48 @@
 $( function() {
+	/* Global Site Scripts */
 	var activenav = $('body').data('active-nav');
-	if('none' != activenav)
-	{
+	if('none' != activenav) {
 		var target = '#nav-' + activenav;
 		$(target).addClass('active');
-	}
+	};
 
-	var ruleFormTemplate = $('#form-__prefix__-row');
-	var ruleFormContainer = $('#ruleform_table');
-	var totalFormsField = $('#id_form-TOTAL_FORMS');
+	/* Progress Bars */
+	$('div.progress-bar').css('width', function () {
+		return $(this).attr('aria-valuenow') + '%';
+	});
 
+	/* Firewall Rules Table */
 	$('#new-port-button').click(function() {
-		var new_form = ruleFormTemplate.clone(true);
-		var new_idx = totalFormsField.val();
+		var new_form = $('#form-__prefix__-row').clone(true);
+		var new_idx = $('#id_form-TOTAL_FORMS').val();
+
+		var updateElementIndex = function(elm, idx) {
+			if(elm.attr('for')) {
+				elm.attr('for', elm.attr('for').replace('__prefix__', idx));
+			}
+			if(elm.attr('id')) {
+				elm.attr('id', elm.attr('id').replace('__prefix__', idx));
+			}
+			if(elm.attr('name')) {
+				elm.attr('name', elm.attr('name').replace('__prefix__', idx));
+			}
+		};
 
 		updateElementIndex(new_form, new_idx);
 		new_form.find('*').each(function(){updateElementIndex($(this), new_idx)});
 
-		ruleFormContainer.append(new_form);
-		totalFormsField.val(parseInt(new_idx)+1);
+		$('#ruleform_table').append(new_form);
+		$('#id_form-TOTAL_FORMS').val(parseInt(new_idx)+1);
 		new_form.find('td').fadeTo(400, 0.7, function(){ $(this).fadeTo(400, 1.0) });
 	});
 
-	var updateElementIndex = function(elm, idx) {
-		if(elm.attr('for'))
-		{
-			elm.attr('for', elm.attr('for').replace('__prefix__', idx));
-		}
-		if(elm.attr('id'))
-		{
-			elm.attr('id', elm.attr('id').replace('__prefix__', idx));
-		}
-		if(elm.attr('name'))
-		{
-			elm.attr('name', elm.attr('name').replace('__prefix__', idx));
-		}
-	};
-
+	/* Select Custom Widget */
 	$('div.selectcustombox>:first-child').change(function() {
 		var elem = $(this);
 		var custom = elem.parent().data('custom');
 		var target = elem.next();
 
-		if(elem.val() == custom)
-		{
+		if(elem.val() == custom) {
 			target.slideDown();
 		} else {
 			target.slideUp();
