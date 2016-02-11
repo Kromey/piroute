a144ea795066401388078985984bc5478f2862d3
Travis Veazey <travisvz@gmail.com>
2016-02-10 17:07:38 -0900

    First pass at Networks model
    Going to create a custom model field for ip_range before going further here, need some validation and normalization logic before proceeding.

--- networks/models.py
+++ networks/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+class Networks(models.Model):
+    POLICIES = (
+            ('reject', 'Reject'),
+            ('drop', 'Ignore'),
+            ('accept', 'Accept'),
+            )
+    name = models.CharField(max_length=30)
+    interface = models.CharField(max_length=10)
+    ip_range = models.CharField(max_length=35)
+    policy = models.CharField("default policy", choices=POLICIES)
+
