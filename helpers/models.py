0732a48beb16c0768cf937644da094c93a75a3ac
Travis Veazey <travisvz@gmail.com>
2016-02-11 15:49:07 -0900

    New IPNetworkField model field
    Still need to incorporate a validator

--- helpers/models.py
+++ helpers/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+
+class IPNetworkField(models.CharField):
+    def __init__(self, *args, **kwargs):
+        kwargs['max_length'] = 18
+        super().__init__(*args, **kwargs)
+
+    def deconstruct(self):
+        name, path, args, kwargs = super().deconstruct()
+        del kwargs['max_length']
+        return name, path, args, kwargs
+
