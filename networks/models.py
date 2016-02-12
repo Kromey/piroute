b9da51a0974263cdb940c5f184d2a295fe7f3bdd
Travis Veazey <travisvz@gmail.com>
2016-02-11 15:49:48 -0900

    Use new IPNetworkField for Networks model

--- networks/models.py
+++ networks/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 
+from helpers import models as helpermodels
+
+
 # Create your models here.
 class Networks(models.Model):
     POLICIES = (
@@ -10,6 +13,6 @@ class Networks(models.Model):
             )
     name = models.CharField(max_length=30)
     interface = models.CharField(max_length=10)
-    ip_range = models.CharField(max_length=35)
-    policy = models.CharField("default policy", choices=POLICIES)
+    ip_range = helpermodels.IPNetworkField()
+    policy = models.CharField("default policy", choices=POLICIES, max_length=6)
 
