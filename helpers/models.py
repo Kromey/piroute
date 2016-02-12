cd28805878328e87a4c2f16d5d912a31805de332
Travis Veazey <travisvz@gmail.com>
2016-02-12 10:08:10 -0900

    Add description to IPNetworkField

--- helpers/models.py
+++ helpers/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 
 from . import validators
@@ -7,6 +8,8 @@ from . import validators
 # Create your models here.
 
 class IPNetworkField(models.CharField):
+    description = _("IP address or network")
+
     def __init__(self, *args, **kwargs):
         kwargs['max_length'] = 18
         self.default_validators = [validators.validate_ipv4_network]
