094572c0ee5d4383d97b398521ae93a60d4e41f0
Travis Veazey <travisvz@gmail.com>
2016-02-12 15:50:39 -0900

    New IPNetworkField form field and tests

--- helpers/forms.py
+++ helpers/forms.py
@@ -1,4 +1,47 @@
+import re
+
+
 from django import forms
+from django.core.exceptions import ValidationError
+
+
+from . import validators
+
+
+class IPNetworkField(forms.Field):
+    ipv4_masked_network_re = re.compile(r'^(25[0-5]|2[0-4]\d|[0-1]?\d?\d)(\.(25[0-5]|2[0-4]\d|[0-1]?\d?\d)){3}/\d{1,3}(\.\d{1,3}){3}\Z')
+    default_validators = [validators.validate_ipv4_network]
+
+    def to_python(self, value):
+        if self.ipv4_masked_network_re.search(value):
+            # We were given a dotted-IP-style netmask
+            # Validate it and transform into a CIDR mask
+            ip, mask = value.split('/')
+            mask = mask.split('.')
+
+            # Convert the mask to an integer so we can "walk" its bits
+            mask_int = (int(mask[0]) << 24) + (int(mask[1]) << 16) + (int(mask[2]) << 8) + int(mask[3])
+
+            # The CIDR mask is the number of bits we count in the netmask
+            cidr_mask = 0
+
+            while mask_int > 0:
+                # Modulo 2 gives us the current left-most bit
+                bit = mask_int % 2
+                # Now shift the whole number right
+                mask_int = mask_int >> 1
+
+                if bit == 1:
+                    # Encountered a bit, add one to our mask
+                    cidr_mask += 1
+                elif cidr_mask > 0:
+                    # We've already encountered at least one bit, means the mask isn't contiguous
+                    raise ValidationError('Bad netmask ({mask})'.format(mask='.'.join(mask)))
+
+            # Reassemble IP with CIDR mask
+            value = '/'.join((ip, str(cidr_mask)))
+
+        return value
 
 
 class PirouteForm(forms.Form):
--- helpers/tests.py
+++ helpers/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from django.core.exceptions import ValidationError
+
+
+from . import forms
+
 
 # Create your tests here.
+class TestIPNetworkField(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.field = forms.IPNetworkField()
+
+    def testSimpleAddress(self):
+        self.assertEqual(self.field.clean('172.16.12.34'), '172.16.12.34')
+
+    def testCIDRMask(self):
+        self.assertEqual(self.field.clean('172.16.0.0/12'), '172.16.0.0/12')
+
+    def testNetmask(self):
+        # Netmasks get rewritten to CIDR masks
+        self.assertEqual(self.field.clean('172.16.0.0/255.240.0.0'), '172.16.0.0/12')
+
+    def testBadCIDRMask(self):
+        self.assertRaisesMessage(ValidationError, 'Enter a valid IPv4 network/address.', self.field.clean, '172.16.0.0/36')
+
+    def testBadNetmask(self):
+        self.assertRaisesMessage(ValidationError, 'Bad netmask', self.field.clean, '172.16.0.0/255.138.0.0')
+        self.assertRaisesMessage(ValidationError, 'Bad netmask', self.field.clean, '172.16.0.0/255.240.0.128')
+
